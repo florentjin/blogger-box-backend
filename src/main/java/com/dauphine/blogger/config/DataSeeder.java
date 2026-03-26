0adee0c33f29a77366fbc810ca3950dd3dcf1e89
flore <florentjin13@gmail.com>
2026-03-26 16:50:21 +0100

    Version fonctionnelle v2

---
+++ src/main/java/com/dauphine/blogger/config/DataSeeder.java
@@ -0,0 +1,12 @@
+package com.dauphine.blogger.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * DataSeeder : DÉSACTIVÉ.
+ * L'utilisateur souhaite utiliser ses propres données depuis Supabase.
+ */
+@Configuration
+public class DataSeeder {
+    // Les fausses données ne seront plus insérées dans la base.
+}
